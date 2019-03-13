dfe8d523dc4f3bfcad3f6fc917435efe63075724
Wang Zhijie <wangzhijie@megvii.com>
2019-03-13 20:10:13 +0800

    Fix Chroma range to [-120, 120]

--- test.m
+++ test.m
@@ -11,10 +11,10 @@ mn=min(rgb,[],3);% min of the 3 colors
 alpha = 0.01 * (mn ./ mx);
 gamma = 3;
 Q = exp(alpha .* gamma);
+% xyz = rgb2xyz(rgb);
+% lab = xyz2lab(xyz);
 % calculate Chroma
-lab = rgb2lab(rgb);
-a = lab(:,:,2);
-b = lab(:,:,3);
+[l,a,b] = rgb2lab_n(rgb);
 Chroma = sqrt(a.^2 + b.^2);
 StdVarianceChroma = std(reshape(Chroma(:,:),[],1));
 
@@ -26,6 +26,6 @@ MeanSaturation = mean(reshape(Saturation(:,:),[],1));
 % calculate luminance
 Luminance = hsv(:,:,3);
 
-ContrastLuminance = max(reshape(Luminance(:,:),[],1)) - min(reshape(Luminance(:,:),[],1));
+ContrastLuminance = max(reshape(l(:,:),[],1)) - min(reshape(l(:,:),[],1));
 
 UCIQE = 0.4680 * StdVarianceChroma + 0.2745 * ContrastLuminance + 0.2576 * MeanSaturation;
